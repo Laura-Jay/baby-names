12d6d096645b08b23bc1cf2dac318ad1e2b65cd0
laura J <laura@academy.com>
2022-04-02 18:18:10 +0100

    finished app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { useState } from "react";
-import Header from "./Header";
 import MainContent from "./MainContent";
 import "./styles.css";
 
 function App(): JSX.Element {
-  const [search, setSearch] = useState("");
-
-  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value);
-  }
-
   return (
     <>
-      <Header searchTerm={search} handleChange={handleSearch} />
-      <MainContent searchTerm={search} />
+      <MainContent />
     </>
   );
 }
--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -4,19 +4,43 @@ import compareTwo from "./compareTwo";
 import NameTile from "./NameTile";
 import FavouriteNameTile from "./FavouriteNameTile";
 
-interface MainContentProps {
-  searchTerm: string;
-}
-
 interface NameData {
   name: string;
+  sex: string;
 }
 
-function MainContent(props: MainContentProps): JSX.Element {
+function MainContent(): JSX.Element {
+  //useState holding input from searchbar
+  const [search, setSearch] = React.useState("");
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
   function searchNames(nameData: NameData) {
-    if (nameData.name.toLowerCase().includes(props.searchTerm.toLowerCase())) {
+    if (nameData.name.includes(search)) {
       return nameData;
     }
+  }
+
+  const [filter, setFilter] = React.useState("");
+
+  function filterGender(nameData: NameData) {
+    if (nameData.sex.includes(filter)) {
+      return nameData;
+    }
+  }
+
+  function handleShowAll() {
+    setFilter("");
+  }
+
+  function handleFilterFemale() {
+    setFilter("f");
+  }
+
+  function handleFilterMale() {
+    setFilter("m");
   }
 
   const allBabyNames = [...babyNamesData];
@@ -33,7 +57,7 @@ function MainContent(props: MainContentProps): JSX.Element {
     isFavourite: false,
   }));
 
-  const [tiles, setTiles] = React.useState(nameTiles.filter(searchNames));
+  const [tiles, setTiles] = React.useState(nameTiles);
 
   const [favourites, setFavourites] = React.useState(favouriteTiles);
 
@@ -65,18 +89,60 @@ function MainContent(props: MainContentProps): JSX.Element {
     />
   ));
 
-  const babyNamesTiles = tiles.map((data) => (
-    <NameTile
-      key={data.id}
-      name={data.name}
-      sex={data.sex}
-      handleClick={() => handleFavourite(data.id)}
-      favourite={data.isFavourite}
-    />
-  ));
+  const babyNamesTiles = tiles
+    .filter(searchNames)
+    .filter(filterGender)
+    .map((data) => (
+      <NameTile
+        key={data.id}
+        name={data.name}
+        sex={data.sex}
+        handleClick={() => handleFavourite(data.id)}
+        favourite={data.isFavourite}
+      />
+    ));
 
   return (
     <>
+      <nav className="navbar">
+        <h2 className="navbar--title">Baby Names</h2>
+
+        <div className="button-bar">
+          {filter === "m" ? (
+            <button className="button-selected">Male</button>
+          ) : (
+            <button onClick={handleFilterMale}>Male</button>
+          )}
+
+          {filter === "f" ? (
+            <button className="button-selected">Female</button>
+          ) : (
+            <button onClick={handleFilterFemale}>Female</button>
+          )}
+
+          {filter === "" ? (
+            <button className="button-selected">All</button>
+          ) : (
+            <button onClick={handleShowAll}>All</button>
+          )}
+        </div>
+
+        <p>
+          <strong>Search:</strong>
+        </p>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Name"
+            name="searchTerm"
+            value={search}
+            onChange={handleSearch}
+            className="navbar--search"
+          />
+          <p>You are searching for {search}</p>
+        </div>
+      </nav>
+
       <h3>Favourites</h3>
       <div className="favourites">{favouriteNames}</div>
       <h3>Suggestions:</h3>
